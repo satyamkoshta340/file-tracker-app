93e5930db85299df7f846ee9ab02ad8242116d79
Satyam Koshta <satyamkoshta340@gmail.com>
2023-05-02 13:32:03 +0530

    modified auth using android

--- src/store/authContext.js
+++ src/store/authContext.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { REACT_APP_URL, REACT_APP_EXPO_CLIENT_ID, REACT_APP_ANDROID_CLIENT_ID, REACY_APP_REDIRECT_URI } from "@env";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { useNavigation } from "@react-navigation/native";
 import * as Google from "expo-auth-session/providers/google";
@@ -22,6 +22,7 @@ const AuthContext = React.createContext({
 
 
 export const AuthContextProvider = (props) => {
+  const [gToken, setGToken] = useState(null);
   const [token, setToken] = useState(null);
   const [firstName, setFirstName] = useState(null);
   const [lastName, setLastName] = useState(null);
@@ -54,45 +55,39 @@ export const AuthContextProvider = (props) => {
     expoClientId: REACT_APP_EXPO_CLIENT_ID,
     androidClientId: REACT_APP_ANDROID_CLIENT_ID,
   });
+
+  useEffect(() => {
+    if (response?.type === "success") {
+      setGToken(response.authentication.accessToken);
+      googleLoginHandler();
+    }
+    else if (response?.type === "dismiss") {
+      Alert("error", "Dismissed");
+    }
+  }, [response, gToken]);
+
   const googleLoginHandler = async () => {
-    await promptAsync()
-      .then(async (response) => {
-        setIsLoading(true);
-        Alert("success", response?.type, "response")
-        if (response?.type === "success") {
-          console.log(response.authentication.accessToken);
-          console.log(`${REACT_APP_URL}/auth/google`)
-          await axios
-            .post(`${REACT_APP_URL}/auth/google`, { googleToken: response.authentication.accessToken })
-            .then(async (res) => {
-              console.log("-----------------------------------")
-              // console.log(res.data)
-              Alert("success", "Login Success", "You logged in successfully!")
-              setToken(res?.data?.token)
-              await AsyncStorage.setItem("token", res?.data?.token);
-              await AsyncStorage.setItem("firstName", res?.data?.user?.firstName);
-              await AsyncStorage.setItem("lastName", res?.data?.user?.lastName);
-              await AsyncStorage.setItem("email", res?.data?.user?.email);
-              await AsyncStorage.setItem("profile", res?.data?.user?.picture);
-            })
-            .catch((err) => {
-              Alert("error", "Login Failure", `${err.response.data?.message}`)
-              console.error("error: ", err.response.data?.message);
-            });
-        }
-        else if (response?.type === "dismiss") {
-          Alert("error", "Dismissed");
-        }
-        else {
-          Alert("error", "Error in Login!")
-        }
-        setIsLoading(false);
+    setIsLoading(true);
+    // console.log(gToken);
+    console.log(`${REACT_APP_URL}/auth/google`)
+    await axios
+      .post(`${REACT_APP_URL}/auth/google`, { googleToken: gToken })
+      .then(async (res) => {
+        console.log("-----------------------------------")
+        // console.log(res.data)
+        Alert("success", "Login Success", "You logged in successfully!")
+        setToken(res?.data?.token)
+        await AsyncStorage.setItem("token", res?.data?.token);
+        await AsyncStorage.setItem("firstName", res?.data?.user?.firstName);
+        await AsyncStorage.setItem("lastName", res?.data?.user?.lastName);
+        await AsyncStorage.setItem("email", res?.data?.user?.email);
+        await AsyncStorage.setItem("profile", res?.data?.user?.picture);
       })
       .catch((err) => {
-        console.log("final error", err.response.data?.message);
-        Alert("error", "Error in Login!")
+        // Alert("error", "Login Failure", `${err.response.data?.message}`)
+        console.error("error: ", err.response.data?.message);
       });
-
+    setIsLoading(false);
   };
 
   const logoutHandler = () => {
@@ -109,7 +104,7 @@ export const AuthContextProvider = (props) => {
     profile: profile,
     isLoggedIn: userIsLoggedIn,
     logout: logoutHandler,
-    googleAuth: googleLoginHandler,
+    googleAuth: promptAsync,
     isLoading: isLoading,
   };
 
